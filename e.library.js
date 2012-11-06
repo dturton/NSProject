90c5df95ec62eee8c519d437573ab31a59c54921
eliseobeltran <eliseobeltran@gmail.com>
2012-11-06 17:39:45 +0800

    @function isBlank
    add isBlank function

--- e.library.js
+++ e.library.js
@@ -24,3 +24,44 @@ var Log = function(type){
 		nlapiLogExecution(type, 'ERROR: ' + errorMsg);
 	}
 }
+
+/**
+ * @name isBlank
+ * @description: Object for validating string value
+ * @param (string) string value
+ * @returns bool
+ * @author Eliseo Beltran 
+ */
+var isBlank = function(str){
+	if((str == '') || (str == null) ||(str == undefined) || (str.toString().charCodeAt() == 32)){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+/**
+ * @name isBlank
+ * @description: Return the difference between two dates
+ * @param (date1)
+ * @param (date2) 
+ * @returns int
+ * @author Eliseo Beltran 
+ */
+var days_between = function(date1, date2) {
+
+    // The number of milliseconds in one day
+    var ONE_DAY = 1000 * 60 * 60 * 24
+
+    // Convert both dates to milliseconds
+    var date1_ms = date1.getTime()
+    var date2_ms = date2.getTime()
+
+    // Calculate the difference in milliseconds
+    var difference_ms = Math.abs(date1_ms - date2_ms)
+
+    // Convert back to days and return
+    return Math.round(difference_ms/ONE_DAY)
+
+}
